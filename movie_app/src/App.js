400039cc130dd989f611c6a5dcc0785c8d5d0ec1
myeongjinKim <ab9010@naver.com>
2019-03-27 18:05:04 +0900

    Lists with map

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,27 +3,44 @@ import './App.css';
 import Movie from './Movie';
 
 const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
+  
+  
+  
 ]
 
 const movieImages = [
-  "https://i.pinimg.com/564x/ed/47/96/ed4796ac6feff9d2a6115406f964c928.jpg",
-  "https://i-h1.pinimg.com/236x/36/77/da/3677dafd602b4fc399fb76c3b8b6c158.jpg",
-  "https://i.pinimg.com/564x/16/5e/78/165e787b0510ff651d956c7fa7b4e142.jpg",
-  "https://i.pinimg.com/564x/de/2c/8e/de2c8e1e4ea03e4b6118f8030953cdfb.jpg"
+  
+  
+  
+  
+]
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://i.pinimg.com/564x/ed/47/96/ed4796ac6feff9d2a6115406f964c928.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://i-h1.pinimg.com/236x/36/77/da/3677dafd602b4fc399fb76c3b8b6c158.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://i.pinimg.com/564x/16/5e/78/165e787b0510ff651d956c7fa7b4e142.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://i.pinimg.com/564x/de/2c/8e/de2c8e1e4ea03e4b6118f8030953cdfb.jpg"
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+        {movies.map(movie => {
+         return <Movie title={movie.title} poster={movie.poster} />
+        })}
       </div>
     );
   }
